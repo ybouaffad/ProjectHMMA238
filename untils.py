b179591d49e7b4ac346ffb04d35b0f9722870c54
Yani <Yani@Yanis-MacBook-Pro.local>
2019-05-23 16:13:30 +0200

    J'ai intégré la fonction jit à la deuxième fonction

--- untils.py
+++ untils.py
@@ -43,3 +43,21 @@ def calcul_nb_voisins_fit(Z):
             + Z[x-1][y] + 0 +Z[x+1][y] \
             + Z[x-1][y+1]+Z[x][y+1]+Z[x+1][y+1]
     return N
+
+
+@jit(nopython=True)
+def iteration_jeu_fit(Z):
+    """
+  Cette fonction est optimisé par jit et calcul un tour du jeu de la vie
+    
+    """
+    forme = len(Z), len(Z[0])
+    N = calcul_nb_voisins_fast(Z)
+    for x in range(1, forme[0] - 1):
+        for y in range(1,forme[1]-1):
+            if (Z[x][y] == 1) and (N[x][y] < 2 or N[x][y] > 3):
+                Z[x][y] = 0
+            elif (Z[x][y] == 0) and (N[x][y] == 3):
+                Z[x][y] = 1
+    return Z
+
