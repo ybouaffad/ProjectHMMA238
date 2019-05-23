85ad6c71b3d9fdbcb61334074a43a30b124ade03
skaci02 <samuel.kaci@etu.umontpellier.fr>
2019-05-23 16:10:41 +0200

    J'ai fait la fonction 1 avec jit pour l'optimiser.

--- untils.py
+++ untils.py
@@ -28,3 +28,18 @@ Cette fonction fait un tour de jeu.
             elif Z[x][y] == 0 and N[x][y] == 3:
                 Z[x][y] = 1
     return Z
+
+
+from numba import jit
+
+
+@jit(nopython=True)
+def calcul_nb_voisins_fit(Z):
+    forme = len(Z), len(Z[0])
+    N = [[0, ] * (forme[0]) for i in range(forme[1])]
+    for x in range(1, forme[0] - 1):
+        for y in range(1, forme[1] - 1):
+            N[x][y] = Z[x-1][y-1]+Z[x][y-1]+Z[x+1][y-1] \
+            + Z[x-1][y] + 0 +Z[x+1][y] \
+            + Z[x-1][y+1]+Z[x][y+1]+Z[x+1][y+1]
+    return N
