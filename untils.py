4e68234b3ea8f455c156cf4dafcb86ce96fa51e3
Yani <Yani@Yanis-MacBook-Pro.local>
2019-05-23 16:08:59 +0200

    J'ai écris la deuxième foncton

--- untils.py
+++ untils.py
@@ -13,3 +13,18 @@ def calcul_nb_voisins(Z):
     return N
 
 
+def iteration_jeu(Z):
+    """
+Cette fonction fait un tour de jeu.
+    
+    """
+    
+    forme = len(Z), len(Z[0])
+    N = calcul_nb_voisins(Z)
+    for x in range(1,forme[0]-1):
+        for y in range(1,forme[1]-1):
+            if Z[x][y] == 1 and (N[x][y] < 2 or N[x][y] > 3):
+                Z[x][y] = 0
+            elif Z[x][y] == 0 and N[x][y] == 3:
+                Z[x][y] = 1
+    return Z
